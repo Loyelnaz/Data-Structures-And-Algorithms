cd5844f5f42796855f90eba86b102fa671778570
Loyel Nazareth <nazloyel59@gmail.com>
2022-09-13 22:31:59 +0530

    binary search

---
+++ Algorithms/Searching/Binary Search/BinarySearch.java
@@ -0,0 +1,35 @@
+package BinarySearch;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        // More test cases to try out
+//        int[] arr = {3, 7, 11, 13, 19, 27};
+//        int[] arr = {3, 7, 11, 13, 19, 20};
+        int[] arr = {3, 7, 11, 19, 27};
+        int key = 27;
+        System.out.println(binarySearch(arr, key));
+    }
+
+    // Complexity would O(logN)
+    private static int binarySearch(int[] arr, int key) {
+        int start = 0, end = arr.length - 1;
+        int index = -1;
+        while (start <= end) {
+            // If start and end are of 2^31-1 size, then the addition would
+            // result in bigger number exceeding the integer limits
+            // int mid = (start + end) / 2;
+
+            // Modify it such that we avoid addition of these two numbers
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] < key) {
+                start = mid + 1;
+            } else if (arr[mid] > key) {
+                end = mid - 1;
+            }
+        }
+
+        return index;
+    }
+}
---
+++ Other Topics/Recursion/WaysToReachNthStair.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+/**
+    Problem - https://www.codingninjas.com/codestudio/problems/count-ways-to-reach-nth-stairs_798650
+ */
+public class WaysToReachNthStair {
+    public static void main(String[] args) {
+        System.out.println(countDistinctWayToClimbStair(5));
+    }
+
+    public static int countDistinctWayToClimbStair(long nStairs) {
+        if (nStairs < 0) {
+            return 0;
+        }
+
+        if (nStairs == 0) {
+            return 1;
+        }
+        // as there are only two ways of getting to the nth stair
+        // (i.e. by taking 1 step OR by taking 2 steps),
+        // we can use a recursive function call that would be for (n-1) and (n-2)
+        return countDistinctWayToClimbStair(nStairs-1) + countDistinctWayToClimbStair(nStairs-2);
+    }
+}
