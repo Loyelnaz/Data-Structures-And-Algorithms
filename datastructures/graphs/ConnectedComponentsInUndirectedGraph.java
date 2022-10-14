ee729d485be474228c8b1afea31df330c179c8de
Loyel Nazareth <nazloyel59@gmail.com>
2022-10-14 08:41:40 +0530

    connected components graph problem

---
+++ datastructures/graphs/ConnectedComponentsInUndirectedGraph.java
@@ -0,0 +1,66 @@
+package datastructures.graphs;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * You have a graph of n nodes. You are given an integer n and an array edges where edges[i] = [ai, bi]
+ * indicates that there is an edge between ai and bi in the graph. Return the number of connected components in the graph.
+ *
+ *
+ */
+public class ConnectedComponentsInUndirectedGraph {
+    public static void main(String[] args) {
+        int[][] edges = {{0,1},{1,2},{3,4}};
+        System.out.println(countComponents(5, edges));
+    }
+
+    /**
+     * Time Complexity - O(E+V), O(E) to generate adjacency list, and O(V) to traverse through each vertices
+     * Space Complexity - O(E+V), O(E) space for adjacency list, and O(V) space for storing visited vertices in an array
+     */
+    public static int countComponents(int n, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adjacencyList = adjacencyList(n, edges);
+        boolean[] visited = new boolean[n];
+        int componentCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == false) {
+                dfs(i, visited, adjacencyList);
+                componentCount++;
+            }
+        }
+        return componentCount;
+    }
+
+    private static void dfs(int startNode, boolean[] visited, ArrayList<ArrayList<Integer>> adjacencyList) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(startNode);
+        visited[startNode] = true;
+
+        while (!stack.empty()) {
+            int node = stack.pop();
+
+            for (int edge : adjacencyList.get(node)) {
+                if (visited[edge] == false) {
+                    stack.push(edge);
+                    visited[edge] = true;
+                }
+            }
+        }
+    }
+
+    private static ArrayList<ArrayList<Integer>> adjacencyList(int n, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adjacencyList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < edges.length; i++) {
+            adjacencyList.get(edges[i][0]).add(edges[i][1]);
+            adjacencyList.get(edges[i][1]).add(edges[i][0]);
+        }
+
+        return adjacencyList;
+    }
+}
--- datastructures/graphs/README.md
+++ datastructures/graphs/README.md
@@ -1,33 +1,33 @@
-#Graph
+# Graph
 * Is a collection of nodes (data and a pointer to next node)
 * The data can be an integer, a boolean, a hash map or your own custom objects
 * Linked list too is a type of graph
 * The nodes of a graph are also called <b>vertices</b>, and the pointers that connect them are called <b>edges</b>.
 
-####Directed Edges:
+#### Directed Edges:
 You can traverse only in one direction i.e. if you're at node A, you can move from A->B, but not from B->A</br>
-####Undirected Edges
+#### Undirected Edges
 You can traverse in both the directions.
-####Connected Component
+#### Connected Component
 A group of nodes that are connected by edges
-####Cyclic Graph
+#### Cyclic Graph
 A path in the edge that visits the same node multiple times
 
 The number of edges that can be used to reach the node is the node's <b>indegree</b>. The number of edges that can be used to exit the node is the node's <b>outdegree</b>. Nodes that are connected by an edge are called <b>neighbors</b>.
 
-###Different ways graphs can be declared:
-####Adjacency List
+### Different ways graphs can be declared:
+#### Adjacency List
 * 2D integer array
 * Nodes numbered from 0 to n-1
 * graph[i] will have all the outgoing edges from the ith node
 * Ex: [[1],[2],[0,3],[]]
 
-####Array of edges
+#### Array of edges
 * Nodes numbered from 0 to n-1
 * It's an array where graph[i]=[x,y], which indicates that there is an edge from x to y
 * Ex: edges = [[0, 1], [1, 2], [2, 0], [2, 3]]
 
-####Adjacency Matrix
+#### Adjacency Matrix
 * Nodes numbered from 0 to n-1 
 * 2D matrix of size n x n
 * If graph[i][j] == 1, that means there is an outgoing edge from node i to node j
